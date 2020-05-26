bcdc5d445aabc6c95fc4700c6904f5833988e863
Matthias Diener <mdiener@illinois.edu>
2020-05-26 11:06:20 -0500

    various fixes

--- install.sh
+++ install.sh
@@ -7,6 +7,13 @@ echo "# This script installs the dependencies for emirge. #"
 echo "#####################################################"
 echo
 
+if [[ ! -f meshmode/setup.py ]]; then
+	echo "ERROR: incomplete git clone. Please run:"
+	echo "  git submodule init && git submodule update"
+	echo "to fetch emirge's submodules."
+	exit 1
+fi
+
 myos=$(uname)
 [[ $myos == "Darwin" ]] && myos="MacOSX"
 
@@ -18,9 +25,22 @@ if [[ ! -d ~/miniforge3 ]]; then
     bash Miniforge3-$myos-$myarch.sh -b
 fi
 
-conda update --all --yes
-conda install --yes pocl
+export PATH=$PATH:~/miniforge3/bin
+export OCL_ICD_VENDORS=$OCL_ICD_VENDORS:~/miniforge3/etc/OpenCL/vendors/
+echo 'export OCL_ICD_VENDORS=$OCL_ICD_VENDORS:~/miniforge3/etc/OpenCL/vendors/' >> ~/.bashrc
 
+echo "Installing conda packages"
+conda init
+conda update --all --yes
+conda install --yes pocl clinfo
+
+echo "Installing pip packages"
 pip install pyvisfile
 
-for module in dagrt grudge leap loopy meshmode; do (cd $module && pip install -e .); done
+for module in dagrt leap loopy meshmode grudge; do (cd $module && pip install -e .); done
+
+echo
+echo "############################################################"
+echo "# Emirge is now installed. Please restart your shell       #"
+echo "# and run mirgecom/wave-eager.py to test the installation. #"
+echo "############################################################"
