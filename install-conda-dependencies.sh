91f3cef1093b8701580b508de3bdc1f05417a1fc
Matthias Diener <mdiener@illinois.edu>
2020-06-08 10:21:15 -0500

    Update install-conda-dependencies.sh

--- install-conda-dependencies.sh
+++ install-conda-dependencies.sh
@@ -5,8 +5,3 @@ set -o nounset -o errexit
 echo "==== Installing Conda packages"
 
 conda install --yes pocl numpy pyvisfile pyopencl islpy
-
-#export OCL_ICD_VENDORS=$MY_CONDA_DIR/etc/OpenCL/vendors/
-
-echo "To use the just-installed pocl, add the following line to your ~/.bashrc "
-echo 'export OCL_ICD_VENDORS=$CONDA_PREFIX/etc/OpenCL/vendors/'
