69407d2e867f6831aebb0a0e88c7f61130fb28d8
Matthias Diener <mdiener@illinois.edu>
2020-06-08 13:42:37 -0500

    branch name

--- version.sh
+++ version.sh
@@ -8,40 +8,55 @@ echo "*** Pip info"
 echo -n "Pip path: "
 which pip && pip freeze || echo "No pip found."
 
-
+echo
 echo "*** Conda info"
 
 echo -n "Conda path: "
-which conda && conda info || echo "No conda found." 
+which conda && conda info || echo "No conda found."
+
+echo
+echo "*** OS info"
+
+which lsb_release >/dev/null && lsb_release -ds
+which sw_vers >/dev/null && echo "MacOSX $(sw_vers -productVersion)"
+uname -a
 
 
+echo
 echo "*** Emirge modules"
 
 function print_git_status {
-	res=""
+    res=""
 
-	# Modified/untracked files?
-	[[ -n $(git status -s) ]] && res+="*"
+    # Modified/untracked files?
+    [[ -n $(git status -s) ]] && res+="*"
 
-	res+="$(basename $PWD)|"
-	
-	res+="$(git log -1 --pretty --oneline)|"
-	res+="$(git log -1 --format=%cd --date=format:"%Y-%m-%d %H:%M")\n"
-	echo $res
+    # Package name
+    res+="$(basename $PWD)|"
+
+    # Branch name
+    res+="$(git describe --all --always| sed s,^heads/,, | sed s,remotes/origin/,,)|"
+
+    # Commit message
+    res+="$(git log -1 --pretty --oneline)|"
+
+    # Date
+    res+="$(git log -1 --format=%cd --date=format:"%Y-%m-%d %H:%M")\n"
+    echo $res
 }
 
 
 MY_MODULES=$(git submodule status | awk '{print $2}' | tr '\n' ' ')
 
-text="Package|Commit|Date\n"
-text+="=======|======|====\n"
+text="Package|Branch|Commit|Date\n"
+text+="=======|======|======|====\n"
 
 for m in $MY_MODULES; do
-	cd $m
+    cd $m
 
-	text+=$(print_git_status)
-	
-	cd ..
+    text+=$(print_git_status)
+
+    cd ..
 done
 
 # Emirge status
