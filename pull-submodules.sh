db75ada4573df164f959abe050afed9c8cb687d0
Matthias Diener <mdiener@illinois.edu>
2020-07-01 16:44:07 -0500

    update all submodules

--- pull-submodules.sh
+++ pull-submodules.sh
@@ -17,7 +17,7 @@ MY_MODULES=$(git submodule status | awk '{print $2}')
 for m in $MY_MODULES; do
     cd $m
 
-    echo $m
+    echo === $m
 
     # Skip directories that have local modifications
     git diff-index --quiet HEAD || { echo "  Skipping update of '$m' due to local modifications."; cd ..; continue; }
