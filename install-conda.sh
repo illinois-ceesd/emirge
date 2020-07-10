6ac93e7119d2fb7fed3c6141620469f0b851d72c
Matthias Diener <mdiener@illinois.edu>
2020-07-10 11:43:38 -0500

    conda: make sure to install even if directory already exists (#41)
    Closes #40.

--- install-conda.sh
+++ install-conda.sh
@@ -10,10 +10,10 @@ myarch=$(uname -m)
 # Use $HOME/miniforge3 by default
 MY_CONDA_DIR=${MY_CONDA_DIR:-$HOME/miniforge3}
 
-if [[ ! -d $MY_CONDA_DIR ]]; then
+if [[ ! -d $MY_CONDA_DIR || ! -x $MY_CONDA_DIR/bin/conda ]]; then
     echo "==== Installing Miniforge in $MY_CONDA_DIR."
     wget -c --quiet https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$myos-$myarch.sh
-    bash Miniforge3-$myos-$myarch.sh -b -p $MY_CONDA_DIR
+    bash Miniforge3-$myos-$myarch.sh -u -b -p $MY_CONDA_DIR
 else
     echo "==== Conda found in $MY_CONDA_DIR, skipping Miniforge installation."
 fi
