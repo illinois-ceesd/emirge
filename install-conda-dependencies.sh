d77e0ad982ccf463956495847ea35253dc5fe1b1
Matthias Diener <mdiener@illinois.edu>
2020-07-24 09:57:46 -0500

    Install MPI

--- install-conda-dependencies.sh
+++ install-conda-dependencies.sh
@@ -7,3 +7,9 @@ echo "==== Installing Conda packages for $(which conda)"
 conda info --envs
 
 conda install --yes pocl pyvisfile
+
+# We need an MPI installation to build mpi4py.
+# Install OpenMPI if none is available.
+if [[ $(which mpicc) ]]; then
+    conda install --yes openmpi
+fi
