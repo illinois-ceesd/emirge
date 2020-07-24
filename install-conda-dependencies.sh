c59b513e1dfed4be7ac7c57b65c6e80be26f8ff6
Matthias Diener <mdiener@illinois.edu>
2020-07-24 13:32:56 -0500

    test

--- install-conda-dependencies.sh
+++ install-conda-dependencies.sh
@@ -11,7 +11,7 @@ conda install --yes pocl pyvisfile
 # We need an MPI installation to build mpi4py.
 # Install OpenMPI if none is available.
 if ! command -v mpicc &> /dev/null ;then
-    conda install --yes mpich mpich-mpicc
+    # conda install --yes mpich mpich-mpicc
 
     mv $MY_CONDA_DIR/envs/dgfem/compiler_compat/ld $MY_CONDA_DIR/envs/dgfem/compiler_compat/ld_donotuse
     # source $MY_CONDA_DIR/bin/activate base
