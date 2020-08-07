810aa0b75b4fad92566d070655186c0b362679a1
Mike Campbell <mtcampbe@illinois.edu>
2020-08-07 11:56:02 -0500

    Update install-src-package.sh
    Co-authored-by: Matthias Diener <mdiener@illinois.edu>

--- install-src-package.sh
+++ install-src-package.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 packagepath=$1
-if [ -z "$packagepath" ]
+if [[ -z "$packagepath" ]]
 then
     printf "Error: No package path given.\n"
 fi
 # See https://github.com/illinois-ceesd/mirgecom/pull/43 for why this is not 'pip install -e .'
 (cd $packagepath && python setup.py develop)
-
