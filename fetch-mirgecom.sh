2ddea6b3d2684f7d1b2e52a19cac98be5c47c60a
Michael T. Campbell <mtcampbe@illinois.edu>
2020-08-07 10:01:33 -0700

    Add usage info to fetch-mirgecom

--- fetch-mirgecom.sh
+++ fetch-mirgecom.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+#
+# This script simply grabs mirgecom from the CEESD repo (if necessary)
+# puts it in <install_directory>/mirgecom, and makes the specified
+# <branchname> current.
+# 
+# Usage: fetch-mirgecom.sh <branchname> <install_directory>
+#
+
 set -o nounset -o errexit
+
 mcbranch=$1
 mcprefix=$2
 # default it to branch=master, install=PWD
