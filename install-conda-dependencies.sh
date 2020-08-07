1cf2b2002f5a099b969a1ac7ddec52970e5d03cb
Matthias Diener <mdiener@illinois.edu>
2020-08-07 09:47:07 -0500

    Update install-conda-dependencies.sh

--- install-conda-dependencies.sh
+++ install-conda-dependencies.sh
@@ -6,7 +6,13 @@ echo "==== Installing Conda packages for $(which conda)"
 
 conda info --envs
 
-conda install --yes pocl-cuda pyvisfile pyopencl islpy
+if [[ $(uname) == "Darwin" ]]; then
+    conda install --yes pocl
+else
+    conda install --yes pocl-cuda
+fi
+
+conda install --yes pyvisfile pyopencl islpy
 
 [[ $(uname -m) == "x86_64" ]] && conda install --yes clinfo
 
