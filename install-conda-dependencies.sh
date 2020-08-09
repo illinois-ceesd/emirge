1a2a054e27b8bfff5dbc4573c97892656520fce8
Michael T. Campbell <mtcampbe@illinois.edu>
2020-08-09 14:13:37 -0700

    Add options to install user-specified packages from requirements-like text file.

--- install-conda-dependencies.sh
+++ install-conda-dependencies.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
 
 set -o errexit
+pkg_file=""
+if [[ ! -z "$1" ]]
+then
+    pkg_file="$1"
+    if [[ ! -f "$pkg_file" ]]
+    then
+        echo "install-conda-dependencies.sh::Error: Package file ($pkg_file) not found."
+        exit 1
+    fi
+fi
 
-echo "==== Installing Conda packages for $(which conda)"
+echo "==== Installing Conda packages with $(which conda)"
 
 conda info --envs
 
-if [[ $(uname) == "Darwin" ]]; then
-    conda install --yes pocl
+if [[ ! -z "$pkg_file" ]] 
+then
+
+    echo "  == Installing packages from file ($pkg_file)."
+    for package in $(cat $pkg_file); do
+        echo "   -- Installing user-custom package ($package)."
+        conda install --yes $package
+    done
+
 else
-    conda install --yes pocl-cuda
-fi
 
-conda install --yes pyvisfile pyopencl islpy
+    if [[ $(uname) == "Darwin" ]]; then
+        conda install --yes pocl
+    else
+        conda install --yes pocl-cuda
+    fi
 
-[[ $(uname -m) == "x86_64" ]] && conda install --yes clinfo
-
-# We need an MPI installation to build mpi4py.
-# Install OpenMPI if none is available.
-if ! command -v mpicc &> /dev/null ;then
-    conda install --yes openmpi openmpi-mpicc
+    conda install --yes pyvisfile pyopencl islpy
+    
+    [[ $(uname -m) == "x86_64" ]] && conda install --yes clinfo
+    
+    # We need an MPI installation to build mpi4py.
+    # Install OpenMPI if none is available.
+    if ! command -v mpicc &> /dev/null ;then
+        conda install --yes openmpi openmpi-mpicc
+    fi
 fi
