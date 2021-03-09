cb5abbabdf8c552c98cde221121e0316c471394d
Matthias Diener <mdiener@illinois.edu>
2021-03-09 17:01:44 -0600

    fix

--- install.sh
+++ install.sh
@@ -147,8 +147,8 @@ fi
 # Required to use pocl on macOS Big Sur
 # (https://github.com/illinois-ceesd/emirge/issues/114)
 if [[ $(uname) == "Darwin" ]]; then
-  $(uname -m) == "x86_64" ]] && conda install --yes clang_osx-64
-  $(uname -m) == "arm64" ]] && conda install --yes clang_osx-arm64
+  [[ $(uname -m) == "x86_64" ]] && conda install --yes clang_osx-64
+  [[ $(uname -m) == "arm64" ]] && conda install --yes clang_osx-arm64
 fi
 
 # Install an environment activation script
