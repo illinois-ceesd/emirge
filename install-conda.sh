9bfeab4be36296b3232bacc5eb66c56f56d9facb
Andreas Klöckner <inform@tiker.net>
2021-05-21 13:33:38 -0500

    Fix quoting in bash miniforge

--- install-conda.sh
+++ install-conda.sh
@@ -11,7 +11,7 @@ MY_CONDA_DIR=${MY_CONDA_DIR:-$HOME/miniforge3}
 if [[ ! -d $MY_CONDA_DIR || ! -x $MY_CONDA_DIR/bin/conda ]]; then
     echo "==== Installing Miniforge in $MY_CONDA_DIR."
     curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$myos-$myarch.sh"
-    bash Miniforge3-$myos-"$myarch.sh" -u -b -p "$MY_CONDA_DIR"
+    bash "Miniforge3-$myos-$myarch.sh" -u -b -p "$MY_CONDA_DIR"
 else
     echo "==== Conda found in $MY_CONDA_DIR, skipping Miniforge installation."
 fi
